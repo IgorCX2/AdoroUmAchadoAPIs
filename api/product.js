14b40028207f44ecb257e53195d6d1edc20d8262
BuildTools <unconfigured@null.spigotmc.org>
2022-07-04 12:49:12 -0300

    arquivos editados

--- api/product.js
+++ api/product.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", async (req, res) =>{
-    try{
-        res.json({
-            status:200,
-            message: "get data has succesg",
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("servererro");
-    }
+
+/**
+ * GET product list.
+ *
+ * @return product list | empty.
+ */
+router.get("/", async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: "Get data has successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
 });
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const product = require('./api/product');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5050;
+const product = require("./api/product");
+
+app.use(express.json({ extended: false }));
+
 app.use("/api/product", product);
-app.listen(PORT, () => console.log(`server em ${PORT}`));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
