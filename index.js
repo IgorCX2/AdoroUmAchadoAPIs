058374f84b2df18e34f6f6616c1f1af01a26498f
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 12:32:55 -0300

    apaguei o outro

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
 const product = require("./api/product");
-const user = require("./api/user");
-app.use(express.json());
 
 app.use("/api/product", product);
-app.use("/api/user", user);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
