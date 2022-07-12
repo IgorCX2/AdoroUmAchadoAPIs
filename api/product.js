7680bf26d4c4ec5b5d451db1e261fbfdee2034f9
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 13:10:30 -0300

    sem os prod

--- api/product.js
+++ api/product.js
@@ -1,30 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const Home = require('../models/Home');
 
-router.get("/", async (req, res) =>{
+router.get("/", async (req, res) => {
   try{
     res.json({
-      status: 200,
-      message: "pçaa",
+      status:200,
+      message: "foi",
     });
   }catch(error){
     console.log(error);
     return res.status(500).send("erro");
   }
 });
-router.get("/all", async (req, res) => {
-  await Home.findAll({
-    attributes: ['id_produto', 'nome_produto', 'categorias', 'capa', 'vendidos', 'valor'],
-  })
-    .then((dataHome)=>{
-      return res.json({
-        dataHome
-      });
-    }).catch(()=>{
-      return res.status(400).json({
-        mensagem: "erro: nenhum valor"
-      });
-    });
-});
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const product = require("./api/product");
+const user = require("./api/user");
+app.use(express.json());
 
 app.use("/api/product", product);
+app.use("/api/user", user);
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
