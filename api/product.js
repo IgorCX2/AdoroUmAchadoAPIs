38cd66d66e9e9eda2b9b6f543baf74ad828a7df8
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 12:49:06 -0300

    sem cad

--- api/product.js
+++ api/product.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Home = require('../models/Home');
-const Productdesc = require('../models/Productdesc');
-var bodyParser = require('body-parser');
 
-router.use(express.json());
-router.use(bodyParser.urlencoded({extended: false}));
-router.use(bodyParser.json());
 router.get("/", async (req, res) => {
   await Home.findAll({
     limit: 5,
@@ -14,7 +9,7 @@ router.get("/", async (req, res) => {
       ['vendidos', 'DESC'],
     ],
     attributes: ['id_produto', 'nome_produto', 'categorias', 'capa', 'vendidos', 'valor'],
-  });
+  })
     .then((dataHome)=>{
       return res.json({
         dataHome
@@ -29,7 +24,7 @@ router.get("/", async (req, res) => {
 router.get("/all", async (req, res) => {
   await Home.findAll({
     attributes: ['id_produto', 'nome_produto', 'categorias', 'capa', 'vendidos', 'valor'],
-  });
+  })
     .then((dataHome)=>{
       return res.json({
         dataHome
