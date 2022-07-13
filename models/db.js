c743fb8a1d2084ff68e5f869d884ca8f5120cb5d
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 22:53:04 -0300

    mudei a insta

--- models/db.js
+++ models/db.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('adoroumachado', 'root', '', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  dialectModule: require('mysql2'),
 });
 sequelize.authenticate()
 .then(()=>{
