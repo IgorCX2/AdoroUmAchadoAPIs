98fe89293926c2d0f21b2d7b612e62dfa9188c9c
BuildTools <unconfigured@null.spigotmc.org>
2022-07-20 12:56:12 -0300

    arquivo carrinho

--- api/product.js
+++ api/product.js
@@ -7,6 +7,14 @@ var bodyParser = require('body-parser');
 router.use(express.json());
 router.use(bodyParser.urlencoded({extended: false}))
 router.use(bodyParser.json())
+const cors = require('cors');
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+  router.use(cors());
+  next();
+});
 router.get("/", async (req, res) => {
   await Home.findAll({
     limit: 5,
